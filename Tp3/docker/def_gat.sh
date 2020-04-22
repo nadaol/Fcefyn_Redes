14417ef245104834dfa49d618338d08cf6d5831e
nadaol <nadaol@hotmail.com>
2020-04-21 21:45:13 -0300

    --

--- Tp3/docker/def_gat.sh
+++ Tp3/docker/def_gat.sh
@@ -2,7 +2,6 @@
 # CONFIGURACION default gateways para hosts y el web server
 
 #H3
-
 sudo docker exec -ti --privileged docker_h3_1 ip route del default
 sudo docker exec -ti --privileged docker_h3_1 ip route add default via 192.168.23.11
 
@@ -11,20 +10,17 @@ sudo docker exec -ti --privileged docker_h1_1 ip route del default
 sudo docker exec -ti --privileged docker_h1_1 ip route add default via 192.168.24.11 dev eth0
 
 #H2
-
 sudo docker exec -ti --privileged docker_h2_1 ip route del default
 sudo docker exec -ti --privileged docker_h2_1 ip route add default via 192.168.22.10 dev eth0
 
 #ws
-
 sudo docker exec -ti --privileged docker_ws_1 ip route del default
 sudo docker exec -ti --privileged docker_ws_1 ip route add default via 192.168.25.11 dev eth0
 
 #R2 :para no tener que redistribuir bgp a ospf agrego ruta estatica para el trafico del servidor hacia AS102-AS103
-
 sudo docker exec -ti --privileged docker_r2_1 ip route del default
 sudo docker exec -ti --privileged docker_r2_1 ip route add default via 192.168.15.12 dev eth0
-#agrego ruta estatica para no tener que correr ospf en la interface de h1 (y asi hacerlo privado)para el trafico del servidor a h1
+#agrego ruta estatica para no tener que correr ospf en la interface de h1 (y asi privatizarlo)para el trafico del servidor a h1
 sudo docker exec -ti --privileged docker_r2_1 ip route add 192.168.24.0/24 via 192.168.16.11
 
 #R3 :para no tener que redistribuir bgp a ospf agrego ruta estatica para el trafico del servidor hacia AS102-AS103
@@ -37,6 +33,6 @@ sudo docker exec -ti --privileged docker_r4_1 ip route add default via 192.168.1
 
 #R5 :para no tener que redistribuir ospf a bgp agrego ruta estatica para el trafico de AS103 hacia AS101
 sudo docker exec -ti --privileged docker_r5_1 ip route del default
+#sudo docker exec -ti --privileged docker_r5_1 ip route add default via 192.168.13.12 dev eth2
 sudo docker exec -ti --privileged docker_r5_1 ip route add default via 192.168.11.12 dev eth2
-
 exit
