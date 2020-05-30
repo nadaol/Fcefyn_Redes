c9ad0aeadcf8cbc957093d4fcc0fca8c2a109d0d
nadaol <nadaol@hotmail.com>
2020-05-30 04:14:15 -0300

    topology,dns config,ipv6

---
+++ Tp6/config/configure.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# CONFIGURACION default gateways para hosts y el web server
+
+#H1
+sudo docker exec -ti --privileged h1 ip route del default
+sudo docker exec -ti --privileged h1 ip route add default via 20.0.0.2
+sudo docker exec -ti --privileged h1 ip -6 route del default
+sudo docker exec -ti --privileged h1 ip -6 route add default via 20::2
+
+#H3
+sudo docker exec -ti --privileged h2 ip route del default
+sudo docker exec -ti --privileged h2 ip route add default via 20.0.0.2
+sudo docker exec -ti --privileged h2 ip -6 route del default
+sudo docker exec -ti --privileged h2 ip -6 route add default via 20::2
+
+#prim_com
+sudo docker exec -ti --privileged prim_com ip route del default
+sudo docker exec -ti --privileged prim_com ip route add default via 30.0.0.2
+sudo docker exec -ti --privileged prim_com ip -6 route del default
+sudo docker exec -ti --privileged prim_com ip -6 route add default via 30::2
+
+#R1
+sudo docker exec -ti --privileged r1 ip -6 route del default
+sudo docker exec -ti --privileged r1 ip route del default
+#sudo docker exec -ti --privileged r1 ip route add default via 30.0.0.3
+
+exit
