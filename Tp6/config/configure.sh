9653242f1f06668d2b9bae00d01686bb69b68663
nadaol <nadaol@hotmail.com>
2020-05-30 22:46:50 -0300

    dns reverse zone

--- Tp6/config/configure.sh
+++ Tp6/config/configure.sh
@@ -3,15 +3,15 @@
 
 #H1
 sudo docker exec -ti --privileged h1 ip route del default
-sudo docker exec -ti --privileged h1 ip route add default via 20.0.0.2
+sudo docker exec -ti --privileged h1 ip route add default via 30.1.0.2
 sudo docker exec -ti --privileged h1 ip -6 route del default
-sudo docker exec -ti --privileged h1 ip -6 route add default via 20::2
+sudo docker exec -ti --privileged h1 ip -6 route add default via 30:1::2
 
 #H3
 sudo docker exec -ti --privileged h2 ip route del default
-sudo docker exec -ti --privileged h2 ip route add default via 20.0.0.2
+sudo docker exec -ti --privileged h2 ip route add default via 30.1.0.2
 sudo docker exec -ti --privileged h2 ip -6 route del default
-sudo docker exec -ti --privileged h2 ip -6 route add default via 20::2
+sudo docker exec -ti --privileged h2 ip -6 route add default via 30:1::2
 
 #prim_com
 sudo docker exec -ti --privileged prim_com ip route del default
