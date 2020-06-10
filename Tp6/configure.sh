c35d548eff9c2cb53432b7ec6ce0545f42a059b4
nadaol <nadaol@hotmail.com>
2020-06-10 05:10:46 -0300

    ipv6 Ok

--- Tp6/configure.sh
+++ Tp6/configure.sh
@@ -25,10 +25,23 @@ echo 'prim_com configured Ok'
 sudo docker exec -ti --privileged r1 ip -6 route del default
 sudo docker exec -ti --privileged r1 ip route del default
 sudo docker exec -ti --privileged r1 ip route add default via 40.0.0.6
-#sudo docker exec -ti --privileged r1 ip -6 route add default via 40:0:0:6
+#sudo docker exec -ti --privileged r1 ip -6 route add default via fd17:625c:f037::10
 echo 'r1 configured Ok'
 
+#curl https://server.com -k
+#--cacert /home/server_com.pem
+#en vm2
 #sudo brctl addif br-00154d8b855d enp0s8
-#curl https://40.0.0.5 -H "host:strapi.server.com" -k
+#sudo ip route del 30.0.0.0/24
+#sudo ip route del 30.1.0.0/24
+#sudo ip route add 30.0.0.0/8 via 40.0.0.6 (redes docker -> interfaz router)
 
+#agregar en vm_cluster  
+#sudo ip -6 route add 30::16 via fd17:625c:f037::10 (redes docker ->interfaz router)
+
+#registrar usuario
+#curl --cacert /home/server-com.pem -X POST https://server.com/auth/local/register -H 'Content-Type: application/json' -d '{"username":"user3","email":"user3@gmail.com","password":"123456"}'
+
+#Consulta base de datos
+#curl --cacert /home/server-com.pem -X GET https://server.com/Vehiculos/5ee08d5f13ea4800c26640e0 -H "Authorization: Bearer <token>"
 exit
